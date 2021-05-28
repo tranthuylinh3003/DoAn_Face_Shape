101b3224663cf1fff2b6f01006e7dd281570015a
KhangTran2503 <trankhang2503@gmail.com>
2021-05-28 17:00:11 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # DoAn_Face_Shape
+
+## Link data:
+> ### 1.[Face\_Shape\_Data](./Data/face_shape_data.zip)
+>
+> ### 2.[shape\_predictor\_68\_face\_landmarks.dat](http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2)
+>
+
+## Tham khảo: [MiAI\_Face\_Shape](https://github.com/thangnch/MiAI_Face_Shape)
+
+
+
+
--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 pip install -r requirements.txt
 mkdir models
-wget -P ./models/ http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2 && bunzip2 ./shape_predictor_68_face_landmarks.dat.bz2
-wget https://drive.google.com/file/d/16XGXgfkihNsUnxbZyWmYWwmfG7sGb9u1/view?usp=sharing &&  cp ./face_shape_data/face_data .
+wget -P ./models/ 'http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2'
+upzip ./Data/face_shape_data.zip && mv ./Data/face_shape_data/face_data .
+bunzip2 ./models/shape_predictor_68_face_landmarks.dat.bz2
