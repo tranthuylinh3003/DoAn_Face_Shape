edbf81fc04f899332e2d98e5abfd8f770f5b5df6
KhangTran2503 <trankhang2503@gmail.com>
2021-05-28 21:53:56 +0700

    update 3

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #pip install -r requirements.txt
 #mkdir models
-wget 'http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2' -P ./models/
+#wget 'http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2' -P ./models/
 #unzip ./Data/face_shape_data.zip
-#bzip2 -kfd ./models/shape_predictor_68_face_landmarks.dat.bz2
+bzip2 -kfd ./models/shape_predictor_68_face_landmarks.dat.bz2
