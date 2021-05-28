69d69061b84c3f54e13a7b3212ca581c35f8e3ba
KhangTran2503 <trankhang2503@gmail.com>
2021-05-28 17:30:42 +0700

    Da Train

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 pip install -r requirements.txt
 mkdir models
 wget 'http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2'
-unzip ./Data/face_shape_data.zip && mv ./Data/face_shape_data/face_data .
+unzip ./Data/face_shape_data.zip
 bunzip2 ./shape_predictor_68_face_landmarks.dat.bz2 && mv ./shape_predictor_68_face_landmarks.dat ./models/
