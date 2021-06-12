d2436d9269f0260e8d4f71211f89baebc900cb08
Khang Tran <43286764+KhangTran2503@users.noreply.github.com>
2021-06-13 00:42:39 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ mkdir models
 wget 'http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2'
 mv shape_predictor_68_face_landmarks.dat.bz2 ./models/
 #unzip ./Data/face_shape_data.zip
-!bzip2 -d ./models/shape_predictor_68_face_landmarks.dat.bz2
+bzip2 -d ./models/shape_predictor_68_face_landmarks.dat.bz2
